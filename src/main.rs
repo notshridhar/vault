cdc785371d5509accaa1359e7e2a4520e8df16c8
shridhar <shridhar.me.003@gmail.com>
2022-03-02 21:31:42 +0530

    fix(path): change working dir to app root

--- src/main.rs
+++ src/main.rs
@@ -10,18 +10,22 @@ use crate::error::VaultError;
 use rpassword;
 use std::env;
 use std::io::{self, Write};
+use std::path::Path;
 
 type VaultResult<T> = Result<T, VaultError>;
 
 fn main_app() -> VaultResult<()> {
     let mut args = env::args();
-    let _app_path = args.next().ok_or(VaultError::InvalidPath)?;
+    let app_path = args.next().ok_or(VaultError::InvalidPath)?;
     let command = args.next().ok_or(VaultError::InvalidPath)?;
     let secret_path = args.next().ok_or(VaultError::InvalidPath)?;
 
     let password = rpassword::prompt_password_stdout("password: ").unwrap();
     print!("\x1b[1A\x1b[2K");
     io::stdout().flush().unwrap();
+
+    let app_dir_path = Path::new(&app_path).parent().unwrap();
+    env::set_current_dir(app_dir_path).unwrap();
 
     match command.as_str() {
         "get" => engine::get_secret(&secret_path, &password),
